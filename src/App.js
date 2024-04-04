4f6ee041089b8c06a36e9867e85d88c5c73bb4b9
Rohit Kumar <rohitkumar190204@gmail.com>
2024-04-04 14:36:46 +0530

    Footer remove

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import './App.css';
 import Header from './components/Header/header';
 import Comp from './components/Body/comp';
 import Tool from './components/Tool/tool';
-import Footer from './components/Footer/footer';
 
 function App() {
   return (
